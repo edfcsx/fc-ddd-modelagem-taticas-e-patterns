96303b6323ee08ffbaf3416f25680605cf35a992
Silva, Eduardo Felipe <edfcsx@gmail.com>
2022-11-21 23:36:43 -0300

    add tsc verify and fix main file

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address('Rua dois', '2A', '50800600', 'Recife')
 customer.Address = address
 customer.activate()
 
-const item1 = new OrderItem('1', 'Item1', 10)
-const item2 = new OrderItem('2', 'Item2', 15)
+const item1 = new OrderItem('1', 'Item1', 10, 1, 'p1')
+const item2 = new OrderItem('2', 'Item2', 15, 1, 'p2')
 // eslint-disable-next-line
 const order = new Order('1', '123', [item1, item2])
