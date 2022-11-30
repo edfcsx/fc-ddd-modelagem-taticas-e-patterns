c8ad548e74dd89f96a437aff1fc2f9cf90afab75
Silva, Eduardo Felipe <edfcsx@gmail.com>
2022-11-30 00:30:05 -0300

    add product repository

--- src/infra/repository/product.repository.ts
+++ src/infra/repository/product.repository.ts
@@ -12,14 +12,29 @@ export default class ProductRepository implements ProductRepositoryInterface {
   }
 
   async update (entity: Product): Promise<void> {
-    throw new Error('Method not implemented')
+    await ProductModel.update({
+      name: entity.name,
+      price: entity.price
+    }, { where: { id: entity.id } })
   }
 
   async find (id: string): Promise<Product> {
-    throw new Error('Method not implemented')
+    const productModel = await ProductModel.findOne({ where: { id } })
+
+    return new Product(
+      productModel.id,
+      productModel.name,
+      productModel.price
+    )
   }
 
   async findAll (): Promise<Product[]> {
-    throw new Error('Method not implemented')
+    const productModels = await ProductModel.findAll()
+
+    return productModels.map((productModel) => new Product(
+      productModel.id,
+      productModel.name,
+      productModel.price
+    ))
   }
 }
